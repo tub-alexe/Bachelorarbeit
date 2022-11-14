1bf0e11b5845a542fa40cbffe32fecdb2fcc8fbd
tub-alexe <jeremias@alexe-online.de>
2022-11-14 10:38:45 +0100

    Bisher Expansionsventil, Verdampfer, Überhitzer und Kondensator berücksichtigt, Setzen der Enthalpien funktioniert aber beim Ersetzen durch Temperaturen korrigiert das Programm meine Temperaturen im Gaskühler automatisch

--- Quelle 90 adamson/Kompletter Kreislauf Quelle 90 adamson.py
+++ Quelle 90 adamson/Kompletter Kreislauf Quelle 90 adamson.py
@@ -1,94 +1,103 @@
 from tespy.networks import Network
-from tespy.components import (HeatExchanger, Source, Sink, CycleCloser, Valve, Compressor)
+from tespy.components import (HeatExchanger, Valve, Source, Sink)
 from tespy.connections import Connection
 from CoolProp.CoolProp import PropsSI as CPSI
 
 km = 'R601'
-wa = 'H2O'
-fld_km = {km: 1, wa: 0}
-fld_wa = {km: 0, wa: 1}
+se = 'H2O'
+fld_km = {km: 1, se: 0}
+fld_se = {km: 0, se: 1}
 
-nw = Network(fluids=[km, wa], T_unit='C', p_unit='bar', h_unit='kJ / kg', m_unit='kg / s', Q_unit='kW')
+nw = Network(fluids=[km, se], T_unit='C', p_unit='bar', h_unit='kJ / kg', m_unit='kg / s', Q_unit='kW')
 
 # Komponenten
 
 gk = HeatExchanger('Gaskühler')
 se_ein = Source('Senke ein')
 se_aus = Sink('Senke aus')
-vd_in = Source('Verdampfer rein')
-ue_aus = Sink ('Überhitzer raus')
+
+ue_in = Source('Verdampfer rein')
+vd_aus = Sink('Überhitzer raus')
 vd = HeatExchanger('Verdampfer')
 ue = HeatExchanger('Überhitzer')
-kp = Compressor('Verdichter')
-CC = CycleCloser('Kreislauf')
-va = Valve('Drossel')
+
+exp = Valve('Expansionsventil')
+anfang = Source('Umgebung Eintritt')
+ende = Sink('Umgebung Austritt')
 
 #Verbindungen Kreislauf
 
-c1 = Connection(CC, 'out1', vd, 'in2')
-c2 = Connection(vd, 'out2', ue, 'in2')
-c3 = Connection(ue, 'out2', kp, 'in1')
-c4 = Connection(kp, 'out1', gk, 'in1')
-c5 = Connection(gk, 'out1', va, 'in1')
-c6 = Connection(va, 'out1', CC, 'in1')
+c1 = Connection(anfang, 'out1', gk, 'in1')
+c2 = Connection(gk, 'out1', exp, 'in1')
+c3 = Connection(exp, 'out1', vd, 'in2')
+c4 = Connection(vd, 'out2', ue, 'in2')
+c5 = Connection(ue, 'out2', ende, 'in1')
 
-#Verbindungen Senke und Quelle des Verdampfers und Überhitzers
+#Verbindungen kalte Seite Gaskühler
 
-c7 = Connection(vd_in, 'out1', vd, 'in1')
-c8 = Connection(vd, 'out1', ue, 'in1')
-c9 = Connection(ue, 'out1', ue_aus, 'in1')
+c6 = Connection(se_ein, 'out1', gk, 'in2')
+c7 = Connection(gk, 'out2', se_aus, 'in1')
 
-#Verbindungen Senke und Quelle des Gaskühlers
 
-c10 = Connection(se_ein, 'out1', gk, 'in2')
-c11 = Connection(gk, 'out2', se_aus, 'in1')
+#Verbindungen heiße Seite Verdampfer und Überhitzer
+c8 = Connection(ue_in, 'out1', ue, 'in1')
+c9 = Connection(ue, 'out1', vd, 'in1')
+c10 = Connection(vd, 'out1', vd_aus, 'in1')
+nw.add_conns(c1, c2, c3, c4, c5, c6, c7, c8, c9, c10)
 
-nw.add_conns(c1, c2, c3, c4, c5, c6, c7, c8, c9, c10, c11)
+#Parametrisierung Komponenten
 
-#Parameter Komponenten
-
+gk.set_attr(pr1=1, pr2=1)
 vd.set_attr(pr1=1, pr2=1)
-ue.set_attr(pr2=1)
-gk.set_attr(pr1=1)
-kp.set_attr(eta_s=0.75)
-
-# Parameter Verbindungen Verdampfer und Überhitzer
-
-#Parametrisierung vor dem Verdampfer, Druck im Verdampfer ist auch bekannt sowie wie stark das Fluid überhitzt wird
-
-#bekannter Druck im Verdampfer
-
-h_verd = CPSI("H", "Q", 0, "T", 273.15+70, km) * 1e-3
-c1.set_attr(h=h_verd)
-
-# Zwischen Verdampfer und Überhitzer
-h_zw = CPSI("H", "Q", 1, "T", 273.15+70, km) * 1e-3
-c2.set_attr(h=h_zw, p=2.8)
-
-# Nach dem Überhitzer
-h_uebe = CPSI("H", "P", 2.8 * 1e5, "T", 273.15+75, km) * 1e-3
-c3.set_attr(h=h_uebe, fluid=fld_km)
-
-#Parameter heiße Seite
-c7.set_attr(T=80, m=1, p=1, fluid=fld_wa)
-c9.set_attr(T=75)
-
+ue.set_attr(pr1=1, pr2=1)
 
 # Parametrisierung heiße Seite, vor dem Gaskühler
+
 h_gk_vor = CPSI("H", "P", 36 * 1e5, "T", 273.15+204, km) * 1e-3
-c4.set_attr(h=h_gk_vor)
+c1.set_attr(h=h_gk_vor)
 
 # Parametrisierung heiße Seite, nach dem Gaskühler, Druck bleibt konstant im Gaskühler
 
 h_gk_nach = CPSI("H", "P", 36 * 1e5, "T", 273.15+105, km) * 1e-3
-c5.set_attr(h=h_gk_nach, p=36)
+c2.set_attr(h=h_gk_nach, p=36)
 
-#Parametrisierung kalte Seite
+# Parameter kalte Seite
+# Vor dem Verdampfer
 
-c10.set_attr(T=100, m=1, fluid=fld_wa)
-c11.set_attr(T=200)
+#h_verd = CPSI("H", "Q", 0, "T", 273.15+70, km) * 1e-3
+c3.set_attr(p=2.8)
+
+# Zwischen Verdampfer und Überhitzer
+h_zw = CPSI("H", "P", 2.8 * 1e5, "T", 273.15+70, km) * 1e-3
+c4.set_attr(h=h_zw)
+
+# Nach dem Überhitzer
+h_uebe = CPSI("H", "P", 2.8 * 1e5, "T", 273.15+75, km) * 1e-3
+c5.set_attr(h=h_uebe, fluid=fld_km)
+
+#Parametrisierung kalte Seite Gaskühler
+
+c6.set_attr(T=100, p=20, fluid=fld_se)
+c7.set_attr(T=200)
+
+#Parameter heiße Seite Verdampfer
+c8.set_attr(T=80, m=5, p=5, fluid=fld_se)
+c10.set_attr(T=75)
+
+#Lösen
 
 nw.solve(mode='design')
 nw.print_results()
 
+c1.set_attr(h=None, T=204)
+c2.set_attr(h=None, p=None, T=105)
+c3.set_attr(p=None, x=0)
+c4.set_attr(h=None, T=70, x=1)
+c5.set_attr(h=None, T=75)
 
+
+nw.solve(mode='design')
+nw.print_results()
+
+# h_verd und m von c6 oder c8 entfernen dann funktioniert die Simulation
+# sinnvolles setzen der Massenströme wichtig
